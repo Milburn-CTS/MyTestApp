9cc33d15b36e4bbfdaf305f3114ea4e6806c6a33
MilburnGomes <milburn.gomes@continentaltechsolutions.com>
2017-07-11 23:31:57 +0530

    OdXel Layout

---
+++ app/src/main/java/com/continentaltechsolutions/dell/mytestapp/ServiceActivity.java
@@ -0,0 +1,52 @@
+package com.continentaltechsolutions.dell.mytestapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.continentaltechsolutions.dell.mytestapp.Service.MyService;
+
+
+public class ServiceActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //button objects
+    private Button buttonStart;
+    private Button buttonStop;
+    private Button buttonCall;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+
+        //getting buttons from xml
+        buttonStart = (Button) findViewById(R.id.button3);
+        buttonStop = (Button) findViewById(R.id.button4);
+        buttonCall = (Button) findViewById(R.id.button5);
+
+        //attaching onclicklistener to buttons
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        buttonCall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonStart) {
+            //starting service
+            startService(new Intent(this, MyService.class));
+        } else if (view == buttonStop) {
+            //stopping service
+            stopService(new Intent(this, MyService.class));
+        }
+        else if (view == buttonCall) {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:+917447407748"));
+            startActivity(callIntent);
+
+        }
+    }
+}
